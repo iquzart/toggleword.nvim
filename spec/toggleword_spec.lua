3f2ac4ff3dd76668a6784dc9c09f90bac16790f0
Muhammed Iqbal <iquzart@hotmail.com>
2025-07-24 18:02:28 +0400

    Updated asserts

--- spec/toggleword_spec.lua
+++ spec/toggleword_spec.lua
@@ -1,12 +1,31 @@
--- spec/toggleword_spec.lua
 describe("toggleword", function()
 	local toggleword = require("toggleword")
 
 	it("should toggle 'true' to 'false'", function()
-		assert.are.equal(toggleword.toggles["true"], "false")
+		assert.are.same(toggleword.toggles["true"], { "false" })
 	end)
 
 	it("should toggle 'yes' to 'no'", function()
-		assert.are.equal(toggleword.toggles["yes"], "no")
+		assert.are.same(toggleword.toggles["yes"], { "no" })
+	end)
+
+	it("should toggle 'prod' to 'uat', 'dev', 'preprod'", function()
+		assert.are.same(toggleword.toggles["prod"], { "uat", "dev", "preprod" })
+	end)
+
+	it("should toggle 'dev' to 'preprod', 'prod', 'uat'", function()
+		assert.are.same(toggleword.toggles["dev"], { "preprod", "prod", "uat" })
+	end)
+
+	it("should toggle 'enabled' to 'disabled'", function()
+		assert.are.same(toggleword.toggles["enabled"], { "disabled" })
+	end)
+
+	it("should toggle 'push' to 'pull'", function()
+		assert.are.same(toggleword.toggles["push"], { "pull" })
+	end)
+
+	it("should toggle 'offline' to 'online'", function()
+		assert.are.same(toggleword.toggles["offline"], { "online" })
 	end)
 end)
